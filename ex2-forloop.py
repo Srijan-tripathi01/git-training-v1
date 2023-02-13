b84472aa14d6bf2a8a8b0de1d846a662068e7629
Srijan-tripathi01 <112563178+Srijan-tripathi01@users.noreply.github.com>
2023-02-13 14:00:14 +0530

    updated ex2 with print statement
    Updated the file ex2 with the print statement for the  first time from GitHub

--- ex2-forloop.py
+++ ex2-forloop.py
@@ -2,7 +2,6 @@ def double(n):
     return n*2
 print("The above function returns the double of given number")
 print("Anything else")
-<<<<<<< HEAD
 print("Modification 5th real commit")
-=======
->>>>>>> parent of b8e3461 (5th real commit with modification in ex2)
+
+print("Added this line from thr GitHub website for the first time")
