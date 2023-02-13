2d91bb8eb72f7c6f5c1c0dd337181fb5ffe111b7
Srijan <srijantripathi01@gmail.com>
2023-02-13 12:27:25 +0530

    7th commit:change in ex2

--- ex2-forloop.py
+++ ex2-forloop.py
@@ -2,4 +2,7 @@ def double(n):
     return n*2
 print("The above function returns the double of given number")
 print("Anything else")
+<<<<<<< HEAD
 print("Modification 5th real commit")
+=======
+>>>>>>> parent of b8e3461 (5th real commit with modification in ex2)
